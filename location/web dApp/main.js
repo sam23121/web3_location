c5fbeed3ac97b4c809d00951b0ff321d00696242
sam23121 <smlalene@gmail.com>
2022-07-16 23:15:31 +0300

    add js

--- location/web dApp/main.js
+++ location/web dApp/main.js
@@ -4,6 +4,13 @@ const findMyLocation  = () => {
 
     const success = (postion) => {
         console.log(postion)
+        const latitude = postion.coords.latitude
+        const longtitude = postion.coords.longtitude
+        console.log(latitude + '' + longtitude)
     }
-    const error = "not able to get location"
+    const error = (postion) => {
+        status.textContent= "not able to get location"
+    }
+    navigator.geolocation.getCurrentPosition(su)
+    
 }
